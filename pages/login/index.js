7a26fdfd3de23215f76cbb564569df656b0dfec7
BLANKMYMIND <zhangshunyucn@qq.com>
2020-02-13 18:09:45 +0800

    [feat] 登录页 | 加入动效 & 修改版式

--- pages/login/index.js
+++ pages/login/index.js
@@ -10,108 +10,117 @@ Page({
     password: '',
     uFocus: false,
     pFocus: false,
+    loading: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     setInterval(() => {
       if (that.data.opBackground === '0 0 98rpx rgb(255, 255, 255)') {
-          that.setData({
-            opBackground: '0 0 28rpx rgb(255, 255, 255)',
-          })
-        } else {
-          that.setData({
-            opBackground: '0 0 98rpx rgb(255, 255, 255)',
-          })
-        }
+        that.setData({
+          opBackground: '0 0 28rpx rgb(255, 255, 255)',
+        })
+      } else {
+        that.setData({
+          opBackground: '0 0 98rpx rgb(255, 255, 255)',
+        })
+      }
     }, 600)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  handleU: function (e) {
+  handleU: function(e) {
     this.setData({
       username: e.detail.value,
     })
   },
 
-  handleP: function (e) {
+  handleP: function(e) {
     this.setData({
       password: e.detail.value,
     })
   },
 
-  focusU: function () {
+  focusU: function() {
     this.setData({
       uFocus: true,
     })
   },
 
-  blurU: function () {
+  blurU: function() {
     this.setData({
       uFocus: false,
     })
   },
 
-  focusP: function () {
+  focusP: function() {
     this.setData({
       pFocus: true,
     })
   },
 
-  blurP: function () {
+  blurP: function() {
     this.setData({
       pFocus: false,
     })
+  },
+
+  login: function() {
+    if (this.data.username !== '' && this.data.password !== '') {
+      this.setData({
+        loading: true,
+      })
+    }
   },
 })
